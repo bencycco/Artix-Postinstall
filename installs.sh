fc1125bb2c0dc544e17687e1df086173af805c4f
bencycco <168569357+bencycco@users.noreply.github.com>
2024-06-01 13:46:18 +0100

    Update installs.sh

--- installs.sh
+++ installs.sh
@@ -1,18 +1,4 @@
-echo "Make sure you are running as root."
+echo "make sure this is running with root access"
 read ABC
-echo "Installing Tor Services..."
-pacman -S macchanger tor-runit tor-browser vim
-git clone https://aur.archlinux.org/obfs4proxy
-cd obfs4proxy
-mkpkg -irs
-cd ..
 
-echo "Ready to edit tor config?"
-read ACB
-vim /etc/tor/torrc
-
-echo "Linking Services..."
-ln -s /etc/sv/tor /var/service
-sv restart tor
-
-mv .bashrc ..
+pacman -S clamav tor-browser vim macchanger
